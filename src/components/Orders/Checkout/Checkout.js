1b24b5dbe96b23843743e4add6b153c4104fc13a
hasibkyau <hasibkyau.cse@gmail.com>
2022-02-21 23:53:08 +0600

    added loding

--- src/components/Orders/Checkout/Checkout.js
+++ src/components/Orders/Checkout/Checkout.js
@@ -3,6 +3,7 @@ import { Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import axios from "axios";
+import Spinner from "../../Spinner/Spinner";
 
 const mapStateToProps = state => {
     return {
@@ -18,7 +19,9 @@ class Checkout extends Component {
             deliveryAddress: "",
             phone: "",
             paymentType: "Cash On Delivery",
-        }
+            
+        },
+        isLoading: false,
     }
     inputChangeHandle = (e) => {
         this.setState({
@@ -30,6 +33,9 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
+        this.setState({
+            isLoading: true,
+        })
         const order = {
             ingredients: this.props.ingredients,
             customer: this.state.values,
@@ -37,38 +43,57 @@ class Checkout extends Component {
             orderTime: new Date()
         }
         axios.post("https://burger-builder-723e2-default-rtdb.firebaseio.com/orders.json", order)
-        .then(response=>console.log(response))
-        .catch(err => console.log(err))
+        .then(response=>{
+            if(response === 200){
+                this.setState({
+                    isLoading: false,
+                })
+            }
+            else{
+                this.setState({
+                    isLoading: false,
+                })
+            }
+        })
+        .catch(err => {
+            this.setState({
+                isLoading: false,
+            })
+        })
        // console.log(order);
     }
     render() {
-        return (
-            <div>
-                <h4 style={{
-                    border: "1px solid gray",
-                    boxShadow: "1px 1px #888888",
-                    borderRadious: "5px",
-                    padding: "20px"
-                }}> Payment: {this.props.totalPrice} BDT </h4>
+        let form = <div> 
+            <h4 style={{
+            border: "1px solid gray",
+            boxShadow: "1px 1px #888888",
+            borderRadious: "5px",
+            padding: "20px"
+        }}> Payment: {this.props.totalPrice} BDT </h4>
 
-                <form style={{
-                    border: "1px solid gray",
-                    boxShadow: "1px 1px #888888",
-                    borderRadious: "5px",
-                    padding: "20px"
-                }}>
-                    <textarea onChange={(e) => this.inputChangeHandle(e)} className="form-control" placeholder="Your Address" name="deliveryAddress" value={this.state.values.deliveryAddress}></textarea>
-                    <br />
-                    <input onChange={(e) => this.inputChangeHandle(e)} className="form-control" placeholder="Your Phone Number" name="phone" className="form-control" value={this.state.values.phone}></input>
-                    <br />
-                    <select onChange={(e) => this.inputChangeHandle(e)} name="paymentType" className="form-control" value={this.state.values.paymentType}>
-                        <option value="Cash On Delivery">Cash On Delivery</option>
-                        <option value="bKash">Bkash</option>
-                    </select>
-                    <br />
-                    <Button onClick={this.submitHandler} className="mr-auto" style={{ backgroundColor: "#D70F64" }}>Place Order</Button>
-                    <Link to="/" className="btn btn-secondary m-1">Cancel</Link>
-                </form>
+        <form style={{
+            border: "1px solid gray",
+            boxShadow: "1px 1px #888888",
+            borderRadious: "5px",
+            padding: "20px"
+        }}>
+            <textarea onChange={(e) => this.inputChangeHandle(e)} className="form-control" placeholder="Your Address" name="deliveryAddress" value={this.state.values.deliveryAddress}></textarea>
+            <br />
+            <input onChange={(e) => this.inputChangeHandle(e)} className="form-control" placeholder="Your Phone Number" name="phone" className="form-control" value={this.state.values.phone}></input>
+            <br />
+            <select onChange={(e) => this.inputChangeHandle(e)} name="paymentType" className="form-control" value={this.state.values.paymentType}>
+                <option value="Cash On Delivery">Cash On Delivery</option>
+                <option value="bKash">Bkash</option>
+            </select>
+            <br />
+            <Button onClick={this.submitHandler} className="mr-auto" disabled = {!this.props.purchasable} style={{ backgroundColor: "#D70F64" }}>Place Order</Button>
+            <Link to="/" className="btn btn-secondary m-1">Cancel</Link>
+        </form>
+        </div>
+        return (
+           
+            <div>
+               {this.state.isLoading ? <Spinner/> : form}
             </div>
         );
     }
