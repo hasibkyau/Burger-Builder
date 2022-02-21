026b40f0c09379d2d69c81255b355a61fd5c83aa
hasibkyau <hasibkyau.cse@gmail.com>
2022-02-21 08:53:03 +0600

    added controls.js

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Burger from './Burger/Burger';
+import Controls from './Controls/Controls';
 
 export default class BurgerBuilder extends Component {
     state = {
         ingredients: [
-            { type: 'salad', amount: 1 },
-            { type: 'cheese', amount: 2},
-            { type: 'meat', amount: 1 },
+            { type: 'salad', amount: 0 },
+            { type: 'cheese', amount: 0 },
+            { type: 'meat', amount: 0 },
         ]
     }
     render() {
         return (
-            <div>
+            <div className="d-flex flex-md-row flex-column">
                 <Burger ingredients={this.state.ingredients} />
+                <Controls />
             </div>
         )
     }
---
+++ src/components/BurgerBuilder/Controls/Controls.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Card, CardBody, CardFooter, CardHeader, Button } from 'reactstrap';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
+
+const BuildControl = props => {
+    return (
+        <div className="container">
+            <div className='row'>
+
+                <div className='col-6'>
+                    <div className="ml-5" style={{ fontWeight: "bold", fontSize: "1.2rem", textAlign:"left" }}>{props.label}</div>
+                </div>
+
+                <div className='col-6'>
+                    <button className="btn btn-danger btn-sm m-1">Less</button>
+                    <button className="btn btn-success btn-sm m-1">More</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+
+const Controls = props => {
+    return (
+        <div className="container ml-md-5" style={{ textAlign: "center" }}>
+            <Card style={{
+                marginTop: "30px",
+                marginBottom: "30px",
+                textAlign: "center"
+            }}>
+                <CardHeader style={{
+                    backgroundColor: "#D70F64",
+                    color: "white"
+                }}><h4>Add Ingredients</h4></CardHeader>
+                <CardBody>
+                    {
+                        controls.map(item => {
+                            return <BuildControl
+                                label={item.label}
+                                type={item.type}
+                                key={Math.random()}
+                            />
+                        })
+                    }
+                </CardBody>
+                <CardFooter><h5>Price: BDT</h5></CardFooter>
+            </Card>
+        </div>
+    )
+}
+
+export default Controls;
